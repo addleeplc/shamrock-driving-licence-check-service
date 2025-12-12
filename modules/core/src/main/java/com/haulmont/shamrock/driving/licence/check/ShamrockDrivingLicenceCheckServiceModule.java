30bb75c492f3c511dbc6810f1b82e305a3081c00
root <root@shamrock-dev-environment-service-v1-6d7785c75f-gzq8t>
2025-12-12 10:34:27 +0000

    Initial commit

---
+++ modules/core/src/main/java/com/haulmont/shamrock/driving/licence/check/ShamrockDrivingLicenceCheckServiceModule.java
@@ -0,0 +1,23 @@
+package com.haulmont.shamrock.driving.licence.check;
+
+import com.haulmont.monaco.annotations.Module;
+import com.haulmont.monaco.container.ModuleLoader;
+
+@Module(
+        name = "shamrock-driving-licence-check-service-module",
+        depends = {
+                "monaco-jetty",
+                "monaco-core",
+                "monaco-config",
+                "monaco-graylog-reporter",
+                "monaco-sentry-reporter",
+                "monaco-mq", "monaco-pico-test", "monaco-redis", "monaco-rs", "monaco-unirest"
+        }
+)
+public class ShamrockDrivingLicenceCheckServiceModule extends ModuleLoader {
+
+    public ShamrockDrivingLicenceCheckServiceModule () {
+        super();
+        packages(ShamrockDrivingLicenceCheckServiceModule.class.getPackageName());
+    }
+}
